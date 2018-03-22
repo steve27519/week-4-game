73ae6467c748ec3a4681d3006070e2321eefae2b
steve27519 <stevenyurchuck@gmail.com>
2018-03-21 21:45:18 -0400

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,57 @@
 
     // Select a random number between 19 and 120
-    
+    var userTotal;  
+    var wins;
+    var losses;
+    var random;
     $( document ).ready(function(){
-        var random=Math.floor(Math.random()*101+19)
+    function startGame(){
+        userTotal=0;
+        wins=0;
+        losses=0;
+        random=Math.floor(Math.random()*101+19);
         $('#randomNumber').text(random);
         jewel1= Math.floor(Math.random()*11+1);
         jewel2= Math.floor(Math.random()*11+1);
         jewel3= Math.floor(Math.random()*11+1);
         jewel4= Math.floor(Math.random()*11+1);
-        userTotal= 0;
+        
         $('#finalTotal').text(userTotal);
         
+    }
+       startGame();
     
-    $('#randomNumber').text(random);
+    
     // Each number in jewel 1-12
-    var jewel1= Math.floor(Math.random()*11+1)
-    var jewel2= Math.floor(Math.random()*11+1)
-    var jewel3= Math.floor(Math.random()*11+1)
-    var jewel4= Math.floor(Math.random()*11+1)
+    
     
 
+
     ////Variablea
-  function reset(){
-    Random=Math.floor(Math.random()*101+19);
-    console.log(Random)
-    $('#randomNumber').text(Random);
-    jewel1= Math.floor(Math.random()*11+1);
-    jewel2= Math.floor(Math.random()*11+1);
-    jewel3= Math.floor(Math.random()*11+1);
-    jewel4= Math.floor(Math.random()*11+1);
-    userTotal= 0;
-    $('#finalTotal').text(userTotal);
-    } 
-    var userTotal= 0; 
-    var wins= 0;
-    var losses = 0;
+  
+   
+   
     
   $('#numberWins').text(wins);
   $('#numberLosses').text(losses);
-  
+   
   //Winner
   function winner(){
   alert("Winner!");
     wins++; 
     $('#numberWins').text(wins);
     reset();
+    
   }
   //Loser
   function loser(){
   alert ("You lost");
     losses++;
     $('#numberLosses').text(losses);
-    reset()
+    reset();
   }
   //clicks 
-    $('#one').on ('click', function(){
+    $('#one').on('click', function(){
       userTotal = userTotal + jewel1;
       console.log("New userTotal= " + userTotal);
       $('#finalTotal').text(userTotal); 
@@ -65,8 +62,9 @@
           else if ( userTotal > random){
             loser();
           }   
-    })  
-    $('#two').on ('click', function(){
+    });
+
+    $('#two').on('click', function(){
       userTotal = userTotal + jewel2;
       console.log("New userTotal= " + userTotal);
       $('#finalTotal').text(userTotal); 
@@ -76,7 +74,8 @@
           else if ( userTotal > random){
             loser();
           } 
-    })  
+    });
+
     $('#three').on ('click', function(){
       userTotal = userTotal + jewel3;
       console.log("New userTotal= " + userTotal);
@@ -88,25 +87,31 @@
           else if ( userTotal > random){
             loser();
           } 
-    })  
-    $('#four').on ('click', function(){
+    });
+    $('#four').on('click', function(){
       userTotal = userTotal + jewel4;
       console.log("New userTotal= " + userTotal);
       $('#finalTotal').text(userTotal); 
          
             if (userTotal == random){
             winner();
+            reset();
           }
           else if ( userTotal > random){
             loser();
+            reset();
           }
 
           //resets the game
  
-reset();
-userTotal=0;
-    
-  });
-});
+          
+        });
    
       
+        function reset(){
+           
+          startGame();
+        }
+    });
+
+    
